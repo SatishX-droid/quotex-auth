f32542b58b38c02e527bdb54b0e32d1fcf9cc8ae
SatishX-Droid <dineshdhull339@gmail.com>
2025-03-31 17:12:05 +0530

    Create signal.js

---
+++ pages/signal.js
@@ -0,0 +1,8 @@
+export default function Signal() {
+    return (
+        <div>
+            <h1>Signal Generator</h1>
+            <p>Access granted only to verified traders.</p>
+        </div>
+    );
+}
